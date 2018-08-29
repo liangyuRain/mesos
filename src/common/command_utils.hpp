cab38eed582c86162fed7d0bc6efc2591026f26e
liangyuRain <liangyuzhaorain@hotmail.com>
2018-08-29 16:36:59 -0700

    Windows: Port `wclayer` from `microsoft/hcsshim`.
    Port commands of wclayer into mesos in order to provision Docker image
    on Windows.
    
    Review: https://reviews.apache.org/r/68581

--- src/common/command_utils.hpp
+++ src/common/command_utils.hpp
@@ -17,6 +17,8 @@
 #ifndef __COMMON_COMMAND_UTILS_HPP__
 #define __COMMON_COMMAND_UTILS_HPP__
 
+#include <vector>
+
 #include <process/process.hpp>
 
 #include <stout/option.hpp>
@@ -60,6 +62,80 @@ process::Future<Nothing> untar(
     const Option<Path>& directory = None());
 
 // TODO(Jojy): Add more overloads/options for untar (eg., keep existing files)
+
+
+#ifdef __WINDOWS__
+// Using wclayer from https://github.com/microsoft/hcsshim
+
+/**
+ * creates a new writable container layer
+ *
+ * @param directory layer path
+ * @param layers paths to the read-only parent layers, the order of the layers
+ * matters with base layer should be the last one
+ */
+process::Future<Nothing> wclayer_create(
+    const Path& directory,
+    const std::vector<Path>& layers);
+
+
+/**
+ * exports a layer to a tar file
+ *
+ * @param directory layer path
+ * @param layers paths to the read-only parent layers, the order of the layers
+ * matters with base layer should be the last one
+ * @param output output layer tar
+ * @param compress output with gzip compression
+ */
+process::Future<Nothing> wclayer_export(
+    const Path& directory,
+    const std::vector<Path>& layers,
+    const Path& output,
+    bool gzip = false);
+
+
+/**
+ * imports a layer from a tar file
+ *
+ * @param directory path
+ * @param input input layer tar
+ * @param layers paths to the read-only parent layers, the order of the layers
+ * matters with base layer should be the last one
+ */
+process::Future<Nothing> wclayer_import(
+    const Path& directory,
+    const Path& input,
+    const std::vector<Path>& layers);
+
+
+/**
+ * mounts a scratch
+ *
+ * @param scratch scratch path
+ * @param layers paths to the read-only parent layers, the order of the layers
+ * matters with base layer should be the last one
+ */
+process::Future<Nothing> wclayer_mount(
+    const Path& scratch,
+    const std::vector<Path>& layers);
+
+
+/**
+ * permanently removes a layer directory in its entirety
+ *
+ * @param directory layer path
+ */
+process::Future<Nothing> wclayer_remove(const Path& directory);
+
+
+/**
+ * unmounts a scratch
+ *
+ * @param directory layer path
+ */
+process::Future<Nothing> wclayer_unmount(const Path& directory);
+#endif // __WINDOWS__
 
 
 /**
